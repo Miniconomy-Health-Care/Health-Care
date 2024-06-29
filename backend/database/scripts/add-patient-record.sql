2db5fc9945ef69a55c0ce92e2269945586fe754d
Ama <ama.commey@bbd.co.za>
2024-06-29 20:48:24 +0200

    moved files to another folder, fixed spelling

--- backend/database/scripts/add-patient-record.sql
+++ backend/database/scripts/add-patient-record.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION add_patient_record(new_personalId BIGINT, new_date TEXT)
+CREATE OR REPLACE FUNCTION add_patient_record(new_personaId BIGINT, new_date TEXT)
 RETURNS VOID AS $$
 DECLARE
     treatment_count INTEGER;
     random_treatmentId INTEGER;
 BEGIN
-    INSERT INTO Persona (personalId, isAdmitted) VALUES (new_personalId, true);
+    INSERT INTO Persona (personaId, isAdmitted) VALUES (new_personaId, true);
 
     SELECT COUNT(*) INTO treatment_count FROM Treatment;
 
@@ -13,7 +13,7 @@ BEGIN
     OFFSET floor(random() * treatment_count)::INTEGER
     LIMIT 1;
 
-    INSERT INTO Record (personalId, date, treatmentId) 
-    VALUES (new_personalId, new_date, random_treatmentId);
+    INSERT INTO Record (personaId, date, treatmentId) 
+    VALUES (new_personaId, new_date, random_treatmentId);
 END;
 $$ LANGUAGE plpgsql;
