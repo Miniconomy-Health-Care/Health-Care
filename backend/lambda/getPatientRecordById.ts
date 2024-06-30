105a9357911c9d3f0dc0ff12e023ff4dd0b3193a
Ama <ama.commey@bbd.co.za>
2024-06-30 18:30:39 +0200

    filled lambdas for getting patient records

--- backend/lambda/getPatientRecordById.ts
+++ backend/lambda/getPatientRecordById.ts
@@ -1,8 +1,55 @@
 import {type APIGatewayProxyHandler} from 'aws-lambda';
+import { getSqlPool } from '../utils/dbUtils';
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: event.body || 'Empty Body'
-    };
+
+
+    const pool = await getSqlPool();
+    const patientId = event.pathParameters?.id;
+
+    if (!patientId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing patient ID' }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    }
+
+    try {
+        const query = 'SELECT * FROM patientrecordview WHERE personaId = $1';
+        const result = await pool.query(query, [patientId]);
+        const records = result.rows;
+
+        if (records.length === 0) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'No records found for the given patient ID' }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(records),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        console.error('Error fetching records:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error fetching records', error }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } finally {
+        await pool.end();
+    }
 }
