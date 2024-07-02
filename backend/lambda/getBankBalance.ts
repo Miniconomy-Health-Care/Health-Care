82a4f8ca61df13736241e3db14ea220650d76f47
Ama <ama.commey@bbd.co.za>
2024-07-02 18:18:42 +0200

    get bank balance

--- backend/lambda/getBankBalance.ts
+++ backend/lambda/getBankBalance.ts
@@ -1,8 +1,32 @@
-import {type APIGatewayProxyHandler} from 'aws-lambda';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import {httpsFetch} from '../utils/fetchUtils';
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: event.body || 'Empty Body'
-    };
-}
+    try {
+        const response = await httpsFetch({
+            method: 'GET',
+            host: 'api.commercialbank.projects.bbdgrad.com',
+            path: '/account/balance'
+        });
+
+        if (response.statusCode !== 200) {
+            throw new Error('Failed to request commercial bank');
+        }
+
+        console.log('Successfully retrieved account balance');
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.body)
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        let errorMessage = 'Unknown error occurred';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: errorMessage })
+        };
+    }
+};
