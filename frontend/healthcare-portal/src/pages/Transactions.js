d7dac3a33edfd9d69ecd90070fa53fac8df8c0b9
brittneys20 <brittney.soloman@bbd.co.za>
2024-07-03 21:18:42 +0200

    transactions Table mock json data

--- frontend/healthcare-portal/src/pages/Transactions.js
+++ frontend/healthcare-portal/src/pages/Transactions.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import { Container, Paper, Typography } from '@mui/material';
 import TableTemplate from '../components/TableTemplate';
-
+import { getTransactionRecords } from '../api/api';
 const Transactions = () => {
   const columns = ['ID','Debit Account Name','Credit Account Name','Reference','Amount','Date','Status'];
+  const [transactions,setTransactions] = useState([]);
 
-  const stocks = [
-    { stockid: 1, stocktype: 'Bought', date: '2023-07-01' },
-    { stockid: 2, stocktype: 'Sold', date: '2023-08-15' },
-    { stockid: 3, stocktype: 'Bought', date: '2023-09-20' },
-    // Add more stock records as needed
-  ];
+  useEffect(() => {
+    const sampleJson = `[
+    { "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6","debitAccountName": "string","creditAccountName": "string","reference": "string","amount": 0,"date": "string","status": "failed"}]`;
+
+    const transactionsRecords = JSON.parse(sampleJson);
+
+    const formattedTransactions = transactionsRecords.map(transaction => ({
+      'ID' : transaction.transactionId,
+      'Debit Account Name' : transaction.debitAccountName,
+      'Credit Account Name' : transaction.creditAccountName,
+      'Reference' : transaction.reference,
+      'Amount' : transaction.amount,
+      'Date' : transaction.date,
+      'Status' : transaction.status,
+    }));
+
+    setTransactions(formattedTransactions);
+
+    //Remove the code above once you make sure getTransactionRecords() works when the actual API is ready
+    getTransactionRecords()
+      .then((records) => {
+        const formattedTransactions = records.map(transaction => ({
+          'ID' : transaction.transactionId,
+          'Debit Account Name' : transaction.debitAccountName,
+          'Credit Account Name' : transaction.creditAccountName,
+          'Reference' : transaction.reference,
+          'Amount' : transaction.amount,
+          'Date' : transaction.date,
+          'Status' : transaction.status,
+        }));
+        setTransactions(formattedTransactions);
+      })
+      .catch((err) => console.log(err));
+    }, []);
+
 
   return (
     <Container>
       <Paper className="tablePaper">
         <Typography variant="h4" align="center" className="tableHeading">Transactions Table</Typography>
-        <TableTemplate columns={columns} rows={stocks} />
+        <TableTemplate columns={columns} rows={transactions} />
       </Paper>
     </Container>
   );
