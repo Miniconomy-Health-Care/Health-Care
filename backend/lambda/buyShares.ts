c3330bfc703de066dd9a7116aaa171268e1ba0ef
Work Account <workaccount@Timos-MacBook-Air.local>
2024-07-03 11:12:56 +0200

    working on buy shares lambda

--- backend/lambda/buyShares.ts
+++ backend/lambda/buyShares.ts
@@ -1,5 +1,38 @@
 import {SQSHandler} from 'aws-lambda';
+import {httpsFetch} from '../utils/fetchUtils';
 
-export const handler: SQSHandler = (event) => {
+export const handler: SQSHandler = async (event) => {
     console.log(event);
+
+    const currentBankBalance = await getBankBalance();
+    const balanceForShares = currentBankBalance*0.10;
+
+    const businessMarketValues = await httpsFetch({
+        method: 'GET',
+        host: 'api.mese.projects.bbdgrad.com',
+        path: '/businesses'
+    });
+
+    if (businessMarketValues.statusCode !== 200) {
+        throw new Error('Failed to get market value of businesses from Stock Market');
+    }
+
+    businessMarketValues.body.length = 
+
+
 };
+
+const getBankBalance = async() => {
+
+    const response = await httpsFetch({
+        method: 'GET',
+        host: 'api.commercialbank.projects.bbdgrad.com',
+        path: '/account/balance'
+    });
+
+    if (response.statusCode !== 200) {
+        throw new Error('Failed to get account balance from commercial bank');
+    }
+
+    return response.body.status.accountBalance;
+}
